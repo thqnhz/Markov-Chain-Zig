a9e11e12f372a3ea4d444269a59224f9ca29a003
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2025-09-24 15:11:41 +0700

    feat: gambler's ruin

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+    gamblersRuin(100, 0);
+}
+
+fn gamblersRuin(money: i16, attempts: i16) void {
+    if (money == 0) {
+        std.debug.print(
+            "No more money, you lost it all after {d} attempts! RIP BOZO!\n", .{attempts}
+        );
+        return;
+    }
+    if (money == 200) {
+        std.debug.print("You won the casino after {d} attempts!\n", .{attempts});
+        return;
+    }
+    const win = std.crypto.random.boolean();
+    if (win) {
+        std.debug.print("Attemps: {d} - You won!\nCurrent money: {d} dollars.\n\n", .{attempts, money + 1});
+        return gamblersRuin(money + 1, attempts + 1);
+    }
+    std.debug.print("Attemps: {d} - You lost!\nCurrent money: {d} dollars.\n\n", .{attempts, money - 1});
+    return gamblersRuin(money - 1, attempts + 1);
 }
