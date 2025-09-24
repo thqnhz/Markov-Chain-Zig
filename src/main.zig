8a4a98e52c5f3a774cfaedfb2c9ce64b82e37616
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2025-09-24 15:27:55 +0700

    chore: formatting with zls

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,7 @@ pub fn main() !void {
 
 fn gamblersRuin(money: u8, attempts: u16) void {
     if (money == 0) {
-        std.debug.print(
-            "No more money, you lost it all after {d} attempts! RIP BOZO!\n", .{attempts}
-        );
+        std.debug.print("No more money, you lost it all after {d} attempts! RIP BOZO!\n", .{attempts});
         return;
     }
     if (money == 200) {
@@ -17,9 +15,9 @@ fn gamblersRuin(money: u8, attempts: u16) void {
     }
     const win = std.crypto.random.boolean();
     if (win) {
-        std.debug.print("Attemps: {d} - You won!\nCurrent money: {d} dollars.\n\n", .{attempts, money + 1});
+        std.debug.print("Attemps: {d} - You won!\nCurrent money: {d} dollars.\n\n", .{ attempts, money + 1 });
         return gamblersRuin(money + 1, attempts + 1);
     }
-    std.debug.print("Attemps: {d} - You lost!\nCurrent money: {d} dollars.\n\n", .{attempts, money - 1});
+    std.debug.print("Attemps: {d} - You lost!\nCurrent money: {d} dollars.\n\n", .{ attempts, money - 1 });
     return gamblersRuin(money - 1, attempts + 1);
 }
